91231428c2141bcd339f25c8f1136f871e762a67
XChenZ <thu15chen@foxmail.com>
2023-12-29 23:49:32 +0800

    add utils

--- model/mesh_guide_deformation.py
+++ model/mesh_guide_deformation.py
@@ -1,34 +1,6 @@
 import torch
 import torch.nn as nn
 from model.network.embedder import get_embedder
-from flame.FLAME import FLAME
-
-class Morphable_model(object):
-    def __init__(self, model_type='Flame', device='cuda:0'):
-        assert model_type in ['Flame']
-        self.model_type = model_type
-        if self.model_type == 'Flame':
-            self.model = FLAME().to(device)
-            self.n_shape, self.n_expr, self.n_pose = 100, 50, 15
-            self.cano_verts = self.model.v_template.unsqueeze(0) * 4
-
-    def get_verts(self, param_dict):
-        if self.model_type == 'Flame':
-            verts_p = self.model(shape_params=param_dict['shape'], expression_params=param_dict['exp'],
-                                 full_pose=param_dict['full_pose'], only_vert=True)
-            # CAREFUL: FLAME head is scaled by 4 to fit unit sphere tightly # IMAvatar
-            verts_p *= 4
-            return verts_p
-
-    def split_params(self, param):
-        if self.model_type == 'Flame':
-            assert param.shape[-1] == self.n_shape + self.n_expr + self.n_pose
-            param_dict = {
-                'shape': param[..., :self.n_shape],
-                'exp': param[..., self.n_shape:self.n_shape+self.n_expr],
-                'full_pose':  param[..., self.n_shape+self.n_expr:self.n_shape+self.n_expr+self.n_pose]
-            }
-            return param_dict
 
 def init_weights(m):
     if type(m) == nn.Linear:
